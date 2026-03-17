e0b97d3a2eb66d8f55194b5929e86e2e4c984684
badersalis <badersaadousalissou@gmail.com>
2026-03-17 15:24:29 +0000

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+# Creating directory
+echo "Setting up directories and files..." 
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directories and files created successfully"
